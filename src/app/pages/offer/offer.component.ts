d60c3853d24604a726c6df3c99ae1d42b35acb93
Sunil Salaria <sunilsalaria1993@gmail.com>
2022-04-08 22:37:59 +0530

    offer page
    offer complete

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -12,4 +12,39 @@ export class OfferComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /*this is for reference who we can add value/property manually not via api only static value via compoenent*/ 
+  /*use all cards text properties here and bind these properties with the help of data binding and interpolation in html*/
+  
+  /*plan title personal*/ 
+  planPersonalTitle:any="Personal";
+  planPersonalCurrency:any="$";
+  planPersonalPrice:any="99";
+  planPersonalTime:any="/ monthly";
+  planPersonalDescription:any="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe eveniet similique modi sed, iusto in assumenda inventore omnis facere nisi provident enim officiis quis aspernatur porro dolores adipisci repellendus!";
+  planPersonalGetStartedBtn:any="Get Started";
+
+   /*plan title teams*/ 
+   planTeamsTitle:any="Teams";
+   planTeamsCurrency:any="$";
+   planTeamsPrice:any="199";
+   planTeamsTime:any="/ monthly";
+   planTeamsDescription:any="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe eveniet similique modi sed, iusto in assumenda inventore omnis facere nisi provident enim officiis quis aspernatur porro dolores adipisci repellendus!";
+   planTeamsGetStartedBtn:any="Get Started";
+
+    /*plan title corporate*/ 
+    planCorporateTitle:any="Corporate";
+    planCorporateCurrency:any="$";
+    planCorporatePrice:any="300";
+    planCorporateTime:any="/ monthly";
+    planCorporateDescription:any="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe eveniet similique modi sed, iusto in assumenda inventore omnis facere nisi provident enim officiis quis aspernatur porro dolores adipisci repellendus!";
+    planCorporateGetStartedBtn:any="Get Started";
+
+    /*plan title company*/ 
+    planCompanyTitle:any="Company";
+    planCompanyCurrency:any="$";
+    planCompanyPrice:any="999";
+    planCompanyTime:any="/ monthly";
+    planCompanyDescription:any="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe eveniet similique modi sed, iusto in assumenda inventore omnis facere nisi provident enim officiis quis aspernatur porro dolores adipisci repellendus!";
+    planCompanyGetStartedBtn:any="Get Started";
+
 }
